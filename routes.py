4529b99887ddd1d2bf220e3685064324799069a2
Elsis-Sitati <sitatielsis@gmail.com>
2016-06-14 15:01:35 +0300

    modified create routes

--- routes.py
+++ routes.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask,url_for,request,render_template
 from app import app
 
 #server/
 @app.route('/')
 def hello():
 	return "Hello Geeks"
+
+#server/create
+
+@app.route('/create')
+def create():
+	if request.method == 'GET':
+		#send the user the form
+		return render_template('CreateQuestion.html')
+	elif request.method == 'POST':
+		#read form data and save it
+		title =request.form['title']
+		question =request.form['question']
+		answer =request.form['answer']  
+		#store data in db 
+
+		return render_template('CreatedQuestion.html',question = question) 
+	else:
+		return '<h2>Invalid request</h2>'
 
 # server/question/<title>
 @app.route('/question/<title>')
