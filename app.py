a2e0719a73c8963466f2e6d522b27ebd86de925a
Elsis-Sitati <sitatielsis@gmail.com>
2016-06-17 06:01:48 +0300

    created schema

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 #creating flask object
 from flask import Flask
-from flask_sqlachemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
 
 #create database object
@@ -25,3 +25,19 @@ if __name__ == '__main__':
 	except ValueError:
 		PORT = 5555
 	app.run(HOST,PORT)
+
+#creating database schema
+class AskQuestions (db.Model):
+	id = db.Column(db.Integer,primary_key =True)
+	title=db.Column(db.String(50),unique = False)
+	question=db.Column(db.String(200),unique=False)
+	answer=db.Column(db.String(100),unique=False)
+	
+	def __init__(self,title,question,answer):
+		self.title=title
+		self.question=question
+		self.answer=answer
+
+	def __repr__(self):
+		return '<AskQuestions %r>' % self.title
+
